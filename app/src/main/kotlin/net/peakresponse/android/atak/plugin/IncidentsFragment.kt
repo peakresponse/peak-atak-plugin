93100c63d5bb7c39b648a2754312e5c4b84672e7
Francis Li <mail@francisli.com>
2025-10-11 15:07:21 -0700

    [Closes #5] Displays active MCI incidents after log in

--- app/src/main/kotlin/net/peakresponse/android/atak/plugin/IncidentsFragment.kt
+++ app/src/main/kotlin/net/peakresponse/android/atak/plugin/IncidentsFragment.kt
@@ -5,21 +5,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.atak.plugins.impl.PluginLayoutInflater
 import com.atakmap.coremap.log.Log
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 import net.peakresponse.android.atak.plugin.R
 import net.peakresponse.android.shared.PRAppData
 import net.peakresponse.android.shared.PRSettings
+import net.peakresponse.android.shared.models.IncidentWithScene
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class IncidentsCellHolder(
+val dateFormat = SimpleDateFormat("MMM, d, YYYY  hh:mm a", Locale.US)
+
+class IncidentCellHolder(
     view: View
 ) : RecyclerView.ViewHolder(view) {
     val incidentNumberLabel: TextView
@@ -31,6 +40,48 @@ class IncidentsCellHolder(
         addressLabel = view.findViewById<TextView>(R.id.addressLabel)
         timestampLabel = view.findViewById<TextView>(R.id.timestampLabel)
     }
+
+    fun bind(item: IncidentWithScene) {
+        incidentNumberLabel.text = "#${item.incident.number}"
+        addressLabel.text = item.sceneWithAddress?.address ?: ""
+        item.incident.createdAt?.let { createdAt ->
+            timestampLabel.text = dateFormat.format(createdAt)
+        } ?: run {
+            timestampLabel.text = ""
+        }
+    }
+}
+
+class IncidentWithSceneDiffer : DiffUtil.ItemCallback<IncidentWithScene>() {
+    override fun areItemsTheSame(oldItem: IncidentWithScene, newItem: IncidentWithScene): Boolean {
+        return oldItem.incident.id == newItem.incident.id
+    }
+
+    override fun areContentsTheSame(
+        oldItem: IncidentWithScene,
+        newItem: IncidentWithScene
+    ): Boolean {
+        return oldItem.incident.updatedAt !== newItem.incident.updatedAt || oldItem.sceneWithAddress?.scene?.updatedAt !== newItem.sceneWithAddress?.scene?.updatedAt
+    }
+}
+
+class IncidentsAdapter(
+    val pluginContext: Context
+) : ListAdapter<IncidentWithScene, IncidentCellHolder>(IncidentWithSceneDiffer()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IncidentCellHolder {
+        val view = PluginLayoutInflater.inflate(
+            pluginContext,
+            R.layout.incidents_cell_layout,
+            parent,
+            false
+        )
+        return IncidentCellHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: IncidentCellHolder, position: Int) {
+        val incident = getItem(position)
+        holder.bind(incident)
+    }
 }
 
 class IncidentsFragment(
@@ -39,6 +90,11 @@ class IncidentsFragment(
     companion object {
         private const val TAG = "net.peakresponse.android.atak.plugin.IncidentsFragment"
     }
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var emptyView: View
+    private lateinit var incidentsAdapter: IncidentsAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun getTabTitle(): String {
         return pluginContext.getString(R.string.tab_mcis)
@@ -52,37 +108,15 @@ class IncidentsFragment(
         val view =
             PluginLayoutInflater.inflate(pluginContext, R.layout.incidents_layout, container, false)
 
-        val emptyView = view.findViewById<TextView>(R.id.emptyView)
-        emptyView.visibility = View.GONE
+        progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        emptyView = view.findViewById<TextView>(R.id.emptyView)
+
+        incidentsAdapter = IncidentsAdapter(pluginContext)
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = object : RecyclerView.Adapter<IncidentsCellHolder>() {
-            override fun onCreateViewHolder(
-                parent: ViewGroup,
-                viewType: Int
-            ): IncidentsCellHolder {
-                val view = PluginLayoutInflater.inflate(
-                    pluginContext,
-                    R.layout.incidents_cell_layout,
-                    parent,
-                    false
-                )
-                return IncidentsCellHolder(view)
-            }
-
-            override fun onBindViewHolder(
-                holder: IncidentsCellHolder,
-                position: Int
-            ) {
-                holder.incidentNumberLabel.text = "#0000$position"
-            }
-
-            override fun getItemCount(): Int {
-                return 3
-            }
-        }
-        recyclerView.visibility = View.VISIBLE
+        recyclerView.adapter = incidentsAdapter
         return view
     }
 
@@ -93,16 +127,23 @@ class IncidentsFragment(
             settings.assignmentId?.let { assignmentId ->
                 PRAppData.connectIncidents(it, assignmentId)
             }
-            Log.d(TAG, "Getting db reference")
             val db = PRAppData.getDb(it)
-            Log.d(TAG, "Getting dao reference")
             val dao = db.getIncidentDao()
             viewLifecycleOwner.lifecycleScope.launch {
-                Log.d(TAG, "Querying incidents")
-                val incidentsFlow = dao.queryIncidents()
+                val incidentsFlow = dao.getActiveMciIncidentsWithScenes().distinctUntilChanged()
                 withContext(Dispatchers.IO) {
-                    incidentsFlow.collect {
-                        Log.d(TAG, "incidents=$it")
+                    incidentsFlow.collect { incidents ->
+                        withContext(Dispatchers.Main) {
+                            progressBar.visibility = View.GONE
+                            if (incidents.size > 0) {
+                                emptyView.visibility = View.GONE
+                                recyclerView.visibility = View.VISIBLE
+                            } else {
+                                emptyView.visibility = View.VISIBLE
+                                recyclerView.visibility = View.GONE
+                            }
+                            incidentsAdapter.submitList(incidents)
+                        }
                     }
                 }
             }
