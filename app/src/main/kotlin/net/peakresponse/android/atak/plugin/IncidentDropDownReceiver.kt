364481efc66fbf42159f4b6825ed86f35cb7fb60
Francis Li <mail@francisli.com>
2025-10-22 14:36:38 -0700

    First pass, display markers in custom group layer

--- app/src/main/kotlin/net/peakresponse/android/atak/plugin/IncidentDropDownReceiver.kt
+++ app/src/main/kotlin/net/peakresponse/android/atak/plugin/IncidentDropDownReceiver.kt
@@ -14,7 +14,10 @@ import com.atak.plugins.impl.PluginLayoutInflater
 import com.atakmap.android.dropdown.DropDown
 import com.atakmap.android.dropdown.DropDownReceiver
 import com.atakmap.android.maps.MapView
+import com.atakmap.android.maps.Marker
+import com.atakmap.android.util.IconUtilities
 import com.atakmap.coremap.log.Log
+import com.atakmap.coremap.maps.coords.GeoPoint
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.coroutines.Dispatchers
@@ -25,7 +28,16 @@ import kotlinx.coroutines.withContext
 import net.peakresponse.android.atak.plugin.components.TabFragment
 import net.peakresponse.android.atak.plugin.components.TabFragmentPagerAdapter
 import net.peakresponse.android.shared.PRAppData
+import net.peakresponse.android.shared.models.ReportWithPatient
 import net.peakresponse.android.shared.models.Scene
+
+val MARKER_DRAWABLES = listOf(
+    R.drawable.marker_immediate,
+    R.drawable.marker_delayed,
+    R.drawable.marker_minor,
+    R.drawable.marker_expectant,
+    R.drawable.marker_dead
+)
 
 class IncidentDropDownReceiver(
     mapView: MapView,
@@ -80,6 +92,7 @@ class IncidentDropDownReceiver(
         val db = PRAppData.getDb(mapView.context)
         val incidentDao = db.getIncidentDao()
         val sceneDao = db.getSceneDao()
+        val reportDao = db.getReportDao()
         view.findViewTreeLifecycleOwner()?.let { lifecycleOwner ->
             Log.d(TAG, "found view lifecycleOwner $lifecycleOwner")
             lifecycleOwner.lifecycleScope.launch {
@@ -88,6 +101,7 @@ class IncidentDropDownReceiver(
                         .distinctUntilChanged()
                         .flowWithLifecycle(lifecycleOwner.lifecycle, Lifecycle.State.STARTED)
                     var sceneFlow: Flow<Scene>? = null
+                    var reportsFlow: Flow<List<ReportWithPatient>>? = null
                     incidentFlow.collect { incident ->
                         Log.d(TAG, "incident=$incident")
                         withContext(Dispatchers.Main) {
@@ -102,12 +116,69 @@ class IncidentDropDownReceiver(
                                         lifecycleOwner.lifecycle,
                                         Lifecycle.State.STARTED
                                     )
-                                sceneFlow.collect { scene ->
-                                    Log.d(TAG, "scene=$scene")
+                                launch {
+                                    sceneFlow.collect { scene ->
+                                        Log.d(TAG, "scene=$scene")
+                                        withContext(Dispatchers.Main) {
+                                            (fragments[viewPager.currentItem] as? SceneOverviewFragment)?.setScene(
+                                                scene
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (reportsFlow == null) {
+                            reportsFlow = reportDao.getReportsWithPatients(incidentId)
+                                .distinctUntilChanged()
+                                .flowWithLifecycle(
+                                    lifecycleOwner.lifecycle,
+                                    Lifecycle.State.STARTED
+                                )
+                            launch {
+                                reportsFlow.collect { reports ->
+                                    Log.d(TAG, "reports=$reports")
                                     withContext(Dispatchers.Main) {
-                                        (fragments[viewPager.currentItem] as? SceneOverviewFragment)?.setScene(
-                                            scene
-                                        )
+                                        val mapGroup =
+                                            mapView.rootGroup.findMapGroup(pluginContext.getString(R.string.map_group_friendly_name))
+                                        for (reportWithPatient in reports) {
+                                            var point: GeoPoint? = null
+                                            reportWithPatient.patient?.lat?.let { lat ->
+                                                reportWithPatient.patient?.lng?.let { lng ->
+                                                    point = GeoPoint(lat.toDouble(), lng.toDouble())
+                                                }
+                                            }
+                                            point?.let { point ->
+                                                var marker =
+                                                    mapGroup.deepFindUID(reportWithPatient.report.id) as? Marker
+                                                if (marker == null) {
+                                                    marker =
+                                                        Marker(point, reportWithPatient.report.id)
+                                                    mapGroup.addItem(marker)
+                                                } else {
+                                                    marker.point = point
+                                                }
+                                                marker.title = "#${reportWithPatient.report.pin}"
+                                                Log.d(
+                                                    TAG,
+                                                    "report priority=${reportWithPatient.report.priority}"
+                                                )
+                                                reportWithPatient.report.priority?.let { priority ->
+                                                    IconUtilities.setIcon(
+                                                        pluginContext,
+                                                        marker,
+                                                        MARKER_DRAWABLES[priority],
+                                                        false
+                                                    )
+                                                }
+                                            } ?: run {
+                                                val marker =
+                                                    mapGroup.deepFindUID(reportWithPatient.report.id) as? Marker
+                                                marker?.let { marker ->
+                                                    mapGroup.removeItem(marker)
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
--- app/src/main/kotlin/net/peakresponse/android/atak/plugin/PluginMapComponent.kt
+++ app/src/main/kotlin/net/peakresponse/android/atak/plugin/PluginMapComponent.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import android.content.Intent
 import com.atakmap.android.dropdown.DropDownMapComponent
 import com.atakmap.android.ipc.AtakBroadcast.DocumentedIntentFilter
+import com.atakmap.android.maps.DefaultMapGroup
 import com.atakmap.android.maps.MapView
-import net.peakresponse.android.atak.plugin.R
+import com.atakmap.android.overlay.DefaultMapGroupOverlay
 import net.peakresponse.android.shared.api.PRApiClient
 
 class PluginMapComponent : DropDownMapComponent() {
@@ -13,17 +14,25 @@ class PluginMapComponent : DropDownMapComponent() {
         private const val TAG = "net.peakresponse.android.atak.plugin.PluginMapComponent"
     }
 
-    private var context: Context? = null
-    private var dropDown: PluginDropDownReceiver? = null
+    private lateinit var pluginContext: Context
+    private lateinit var dropDown: PluginDropDownReceiver
+    private lateinit var defaultMapGroup: DefaultMapGroup
+    private lateinit var defaultMapOverlay: DefaultMapGroupOverlay
 
     override fun onCreate(context: Context, intent: Intent?, mapView: MapView) {
         context.setTheme(R.style.ATAKPluginTheme)
         super.onCreate(context, intent, mapView)
-        this.context = context
+        pluginContext = context
 
         PRApiClient.API_URL = BuildConfig.API_URL
 
-        dropDown = PluginDropDownReceiver(mapView, context)
+        defaultMapGroup = DefaultMapGroup(pluginContext.getString(R.string.map_group_friendly_name))
+        defaultMapOverlay = DefaultMapGroupOverlay(mapView, defaultMapGroup)
+
+        mapView.rootGroup.addGroup(defaultMapGroup)
+        mapView.mapOverlayManager.addOverlay(defaultMapOverlay)
+
+        dropDown = PluginDropDownReceiver(mapView, pluginContext)
         val filter = DocumentedIntentFilter()
         filter.addAction(PluginDropDownReceiver.SHOW_PLUGIN, "Show the Plugin drop-down")
         registerDropDownReceiver(dropDown, filter)
